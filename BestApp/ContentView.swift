40964655f5eebbbb10c3374ca1db733b30780658
Nasser Alzuabi <you@example.com>
2020-10-07 06:58:23 -0500

    added .gitignore .gitattributes

---
+++ BestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BestApp
+//
+//  Created by naz on 10/7/20.
+//  Copyright © 2020 nasser. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
